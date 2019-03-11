52b3fcafd98332699fd17f2c0927172454d21b25
Darius <dariusd.thomas@gmail.com>
2019-03-11 17:48:24 -0400

    Added job title

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let Resume = {
-    Name: "Darius Thomas",
+    Name: "Darius Thomas".toUpperCase(),
     Career: "Full-Stack Developer",
     Description: "Ready to learn!",
     "My Interests": ["Software Development","Exercising", "Going Out", "Music","Art","Food"],
     "Previous Experience": ["Two semesters worth of coding experience with MATLAB ","I've dabbled in FreeCodeCamps front-end tutorial work","I've registered for and am currently completing the drills for Covalence full-stack course prepwork"," 3 years experience as a Mechanical engineering student at FAU","5 years experience as a restaurant waiter/Bartender"],
     Company:['Florida Atlantic University','FreeCodeCamp','Covalence','Florida Atlantic University',"TGI Fridays/Duffy's"],
+    "Job Title": ['Student','Student','Student','Student', "Waiter/Bartender"]
     "My Skills": ["BAM: Introductory HTML", "BAM: Introductory CSS","BAM: Introductory JavaScript","BAM: Introductory JQuery","BAM: Introductory Bootstrap","BAM: Introductory React and Redux","BAM: Intermediate MATLAB"]
 };
 
