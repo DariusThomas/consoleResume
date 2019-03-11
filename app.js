e8b1f63cf7ffafb58b77bcbcc3d1ddae3171bce7
Darius <dariusd.thomas@gmail.com>
2019-03-11 17:19:03 -0400

    Console resume part 1

---
+++ app.js
@@ -0,0 +1,9 @@
+let Resume = {
+    Name: "Darius Thomas",
+    Career: "Full-Stack Developer",
+    Description: "Ready to learn!",
+    "My Interests": ["Software Development","Exercising", "Going Out", "Music","Art","Food"],
+    "Previous Experience": ["two semesters worth of coding experience with MATLAB ","Dabbled in FreeCodeCamps front-end tutorial work","I've registered for and am currently completing the drills for Covalence full-stack course prepwork"," 3 years experience as a Mechanical engineering student at FAU","5 years experience as a restaurant waiter/Bartender"],
+    "My Skills": ["Introductory HTML", "Introductory CSS","Introductory JavaScript","Introductory JQuery","Introductory Bootstrap","Introductory React and Redux","Intermediate MATLAB"]
+};
+
