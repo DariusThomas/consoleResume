0f5b7f104463c9db4c22514ebbf6109d81d89168
Darius <dariusd.thomas@gmail.com>
2019-03-11 17:32:20 -0400

    added company names

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ let Resume = {
     Career: "Full-Stack Developer",
     Description: "Ready to learn!",
     "My Interests": ["Software Development","Exercising", "Going Out", "Music","Art","Food"],
-    "Previous Experience": ["two semesters worth of coding experience with MATLAB ","Dabbled in FreeCodeCamps front-end tutorial work","I've registered for and am currently completing the drills for Covalence full-stack course prepwork"," 3 years experience as a Mechanical engineering student at FAU","5 years experience as a restaurant waiter/Bartender"],
-    "My Skills": ["Introductory HTML", "Introductory CSS","Introductory JavaScript","Introductory JQuery","Introductory Bootstrap","Introductory React and Redux","Intermediate MATLAB"]
+    "Previous Experience": ["Two semesters worth of coding experience with MATLAB ","I've dabbled in FreeCodeCamps front-end tutorial work","I've registered for and am currently completing the drills for Covalence full-stack course prepwork"," 3 years experience as a Mechanical engineering student at FAU","5 years experience as a restaurant waiter/Bartender"],
+    Company:['Florida Atlantic University','FreeCodeCamp','Covalence','Florida Atlantic University',"TGI Fridays/Duffy's"],
+    "My Skills": ["BAM: Introductory HTML", "BAM: Introductory CSS","BAM: Introductory JavaScript","BAM: Introductory JQuery","BAM: Introductory Bootstrap","BAM: Introductory React and Redux","BAM: Intermediate MATLAB"]
 };
 
